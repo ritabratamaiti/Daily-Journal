1a6e7a2bb130cd5f2db3ed6500c3887634305c84
Ritabrata Maiti <ritabratamaiti@gmail.com>
2022-12-03 23:55:06 +0800

    Added new entry

---
+++ helpers.py
@@ -0,0 +1,19 @@
+# import datetime
+from datetime import datetime
+import json
+
+# temp = {
+#     "latest": 1,
+#     "posts": [
+#         {"1": {
+#             "title": "Coded my personal daily journal",
+#             # todays date
+#             "date": datetime.now().strftime("%d/%m/%Y"),
+#             "display_date": datetime.now().strftime("%b %d, %Y"),
+#         }}
+#     ]
+# }
+
+# # save the data to a json file
+# with open("data.json", "w") as f:
+#     json.dump(temp, f, indent=4)
