32de5c0b74f28dcb9dcff52c910dc82b2c1b1c16
Ritabrata Maiti <ritabratamaiti@gmail.com>
2022-12-08 00:58:33 +0800

    Added a post

--- helpers.py
+++ helpers.py
@@ -64,12 +64,8 @@ def save_post(title, textHTML):
     with open(f"{latest}.json", "w") as f:
         json.dump(post_data, f, indent=4)
 
-title = "Some updates on Serenebase and my generative art project"
-textHTML = """<p>Today, I spent the day focused on further developing <a href="https://www.serenebase.com/">Serenebase</a>, my online platform for mental health resources. I added several new blogs to the site, providing even more information and support for those looking to improve their mental health.</p>
-<p>In addition, I came up with a comprehensive content marketing plan for <a href="https://viberooms.serenebase.com/">viberooms</a>, a new feature on Serenebase that allows users to connect with others in a virtual support group. I am excited to start implementing this plan and helping even more individuals find the support they need.</p>
-<p>I also made the decision to reach out to mental health startup founders this week to add their resources to the directory on Serenebase. I believe that by providing a wide range of options, we can help even more people find the support and tools they need to improve their mental health.</p>
-<p>Lastly, after a brief hiatus, I am excited to return to my generative art project. I am determined to continue working on this project and improving my overall productivity in order to make a positive impact on the mental health community.</p>
-<p>Overall, it was a productive and fulfilling day, and I am looking forward to continuing to grow and improve Serenebase in the coming days and weeks.</p>"""
+title = "7th December 2022"
+textHTML = """<p>Did not achieve much today, but did come up with a plan to market Viberooms</p>"""
 
 def remove_newlines(text):
     return text.replace("\r", "").replace("\n", "")
